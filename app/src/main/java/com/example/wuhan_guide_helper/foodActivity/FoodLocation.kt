40d6dfdd501e631678da9c46f70cc89aba0b7fa9
MOYUNXUAN <112694421+MOYUNXUAN4@users.noreply.github.com>
2025-01-19 18:01:53 +0800

    11

--- app/src/main/java/com/example/wuhan_guide_helper/foodActivity/FoodLocation.kt
+++ app/src/main/java/com/example/wuhan_guide_helper/foodActivity/FoodLocation.kt
@@ -1,9 +1,7 @@
 package com.example.wuhan_guide_helper.foodActivity
 
-import android.content.Intent
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
@@ -13,11 +11,9 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
